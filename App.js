8f7e29a61336fab92d566ddb242b521ed69d70e7
Amitmali04 <amitmali978@gmail.com>
2023-01-24 15:50:41 +0530

    Project Done

--- App.js
+++ App.js
@@ -1,1 +1,26 @@
-
+$("#save").click(function () {
+	$(".validation").remove();
+	var email = $("#email").val();
+	var pass = $("#pass").val();
+	$("#e_error").text("");
+	$("#p_error").text("");
+	if (email == "") {
+		$("#email").addClass("input_error");
+		$("#email").parent().append("<label class='validation'>*Please enter email</label>");
+		return;
+	}
+	if (pass == "") {
+		$("#pass").parent().append("<label  class='validation'>*Please enter password</label>");
+		$("#pass").addClass("input_error");
+		return;
+	}
+	
+});
+$("input[type=text]").focus(function () {
+	$("#email").removeClass("input_error");
+	$(this).next().remove();
+});
+$("input[type=password]").focus(function () {
+	$(this).removeClass("input_error");
+	$(this).next().remove();
+});
